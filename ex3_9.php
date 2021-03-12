a968b9b30c846b709216e8f81e8444598627ddcf
s19t15910 <s19t15910@stu.edu.tw>
2021-03-12 22:37:00 +0800

    0312

---
+++ ex3_9.php
@@ -0,0 +1,26 @@
+<?php
+//PHP 運算子種類與用法
+  $a = 10;
+  $b = 20;
+  echo '$a = 10'."\n";
+  echo '$b = 20'."\n";
+  echo '$a + $b = '.($a + $b)."\n";
+  echo '$a - $b = '.($a - $b)."\n";
+  echo '$a * $b = '.($a * $b)."\n";
+  echo '$a / $b = '.($a / $b)."\n";
+  echo '$a++ = '.($a++)."\n";
+  echo '$a = '.($a)."\n";
+  echo '++$a = '.(++$a)."\n";
+  echo '$a = '.($a)."\n";
+  echo '$b % $a = '.($b%$a)."\n";
+  echo '$a > $b '.($a > $b ? 'true' : 'false')."\n";
+  echo '$a += 1 ==> '.($a += 1)."\n";
+  echo '$a = '.($a)."\n";
+  echo '$a *= $b ==> '.($a *= $b)."\n";
+  echo '$a = '.($a)."\n";
+  echo '$a and $b ==> '.($a and $b)."\n";
+  echo '$a or $b ==> '.($a or $b)."\n";
+
+  
+
+?>
