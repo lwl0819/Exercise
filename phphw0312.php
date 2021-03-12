405a8c62a692ff74a0b6190ef105894cb4a5f40b
s19t15910 <s19t15910@stu.edu.tw>
2021-03-12 15:30:48 +0800

    0312

--- phphw0312.php
+++ phphw0312.php
@@ -17,5 +17,33 @@
     $a = $a + 100;
     echo '$a: '.$a.'<b />';
 
+    $x = 1;
+     function hello($x){
+       $sum = $x * 2;
+       echo "sum=" .$sum.  "函數執行結束 <br />";
+       return $sum;
+     }
+?>
+<p> -------我是分隔線------</p>
+<?php
+     $sum=hello($x);
+     echo "x=$x<br />";
+     echo "sum=$sum<br />"."\n";
+
+
+     
+     //circule rate 
+     define('PI' ,3.1415926);
+
+     //circule radius
+     $radius =10;
+     //caculate circule Area
+     $area = $radius * $radius* PI;
+     echo "Circule area is ".$area."\n";
+
+     //cacule circule length
+     $length =$radius * 2 * PI;
+     echo "Circule length is ".$length."\n";
+
 
 ?>
