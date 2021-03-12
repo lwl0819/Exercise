b6d34408a1162c745a0f7d05fddb8e837634bc80
s19t15910 <s19t15910@stu.edu.tw>
2021-03-12 14:40:57 +0800

    homework 0312

--- phphw0312.php
+++ phphw0312.php
@@ -3,6 +3,19 @@
     print("Hello world"."\n");
     print("This is a frist program"."\n");
     print time();
+    $now =time();
+    print date("\n".'Y-M-D H:i:s');
+    sleep(5);
+    $next =time();
+    print "\n"."Travel Time: ".($next-$now)." seconds!";
+
+    echo '$a: '.$a.'<br />';
+    $name = 100;
+    echo '$name: '.$name.'<br />';
+    $name = 'David';
+    echo '$name: '.$name.'<br />';
+    $a = $a + 100;
+    echo '$a: '.$a.'<b />';
 
 
 ?>
