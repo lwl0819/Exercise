94e7e6cf3b6c2a25612aebbcf3d13084b8a92d5a
s19t15910 <s19t15910@stu.edu.tw>
2021-06-04 14:26:34 +0800

    0604

---
+++ 20210604/getuser.php
@@ -0,0 +1,13 @@
+<?php
+print 'Hello, '. $_COOKIE['username'];
+print ' ';
+
+print 'Hello, '. $_COOKIE['short-user'];
+print ' ';
+
+print 'Hello, '. $_COOKIE['longer-user'];
+print ' ';
+
+print 'Hello, '. $_COOKIE['date-user'];
+print ' ';
+?>
---
+++ 20210604/usertimes.php
@@ -0,0 +1,12 @@
+<?php
+// cookie 一小時後過期
+setcookie('short-user','Peter1',time()+60*60);
+
+// cookie 一天後過期
+setcookie('longer-user','Peter2',time()+60*60*24);
+
+// cookie 指定日期過後，才會過期
+$datetime = microtime(date('2021-06-01 00:00:00')) -microtime(time());
+//print $datetime;
+setcookie('date-user','Peter3',$datetime);
+?>
